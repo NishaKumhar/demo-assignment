5f77dac6c7e883ed630b4ed2033b3b2768fce99f
NishaKumhar <aks6382@gmail.com>
2022-11-05 09:24:52 +0530

    xcv

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
       <div className='row'>
         <div className=' col-lg-2 col-md-4 col-sm-6 col-12'>
           <Sidebar />
+          
         </div>
         <div className='col-lg-10 col-md-4 col-sm-6 col-12'>
           <Content/>
